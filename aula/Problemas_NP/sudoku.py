1279d63a5e05300d5f91418ef87897cb2c471eea
Lucas Gabriel <lucas.espiritosanto@edu.unifil.br>
2021-04-03 15:36:22 -0300

    Adicionado arquivos sudoku.py, Jogo da Velha.py e modificado problemas NP.py

---
+++ aula/Problemas_NP/sudoku.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class TabuleiroSudoku:
+    def __init__(self, numeros_preenchidos) -> None:
+        pass
+
+    def is_solucao(self, numeros_solucao: List[int]) -> bool:
+        pass
+
+
+def _existe_solucao_para(expr: TabuleiroSudoku, numeros_a_preencher: list) -> bool:
+    pass
